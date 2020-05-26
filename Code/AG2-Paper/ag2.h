9da5eff89323f99bc3b1e08696b47e842ccd2ba7
geflx <gabrielfelix2500@gmail.com>
2020-05-26 17:09:50 -0300

    Corr. Obj F

--- Code/AG2-Paper/ag2.h
+++ Code/AG2-Paper/ag2.h
@@ -2,6 +2,7 @@
 #define AG2_H
 
 #include <fstream>
+#include <assert.h>
 #include <limits>
 #include <iostream>
 #include <vector>
@@ -14,6 +15,21 @@
 #define MUTATION_OPTION 0
 
 using namespace std;
+
+struct data{
+    bool job; //If it is a job: 1, otherwise it is a Vehicle
+    int id;  //Job or vehicle identification
+    data(){
+        job=true; //By default, the position is a job.
+        id=-1;
+    }
+};
+
+struct vehicleLoaded{
+    int id;
+    int initialPos;
+    int finalPos;
+};
 
 struct Solution{
 
@@ -38,14 +54,161 @@ void randomSolution(Solution &S, int N, int K)
     }
 }
 
+
+double objFunction(const vector<vehicleLoaded> &vehicleOrder, const vector<data> &configuration,int ncars, int njobs, const vector<vector<int> > &time,
+    const vector<double> &weight,const vector<int> &jobTardiness, const vector<int> &carPrices){
+
+    int travelCosts=0;
+    int vehicleCosts=0;
+    double penaltyCosts=0;
+
+    //First part of Obj Function, calculating the travel costs between origin->customers1->customers2 etc
+    for(int i=0;i<vehicleOrder.size();i++){
+        int initialIndex=vehicleOrder[i].initialPos; //index of first job carried by that car.
+        int whichJob= configuration[initialIndex].id;
+        travelCosts+=time[0][whichJob+1]; // Origin to the first job
+
+        int finalIndex=vehicleOrder[i].finalPos; //index of last job carried by that car.
+
+
+        for(int j=initialIndex;j<finalIndex;j++){
+            int jobA= configuration[j].id;
+            int jobB= configuration[j+1].id;
+            travelCosts+= time[ jobA+1 ][ jobB+1];
+        }
+
+        whichJob= configuration[finalIndex].id;
+        travelCosts+= time[ whichJob+1][ 0 ] ; //Last job to origin
+    }
+
+    //Second part of Obj Function, looking to the cars' prices
+    for(int k=0;k<vehicleOrder.size();k++){
+
+        vehicleCosts+= carPrices[ vehicleOrder[k].id ];
+    }
+
+    for(int i=0;i<njobs;i++){
+
+        penaltyCosts+= jobTardiness[i]* weight[i];
+    }
+
+  //   cout<<"Job Tardiness: "<<endl;
+  //   for(const int &u: jobTardiness){
+  //   	cout<<u<<" ";
+  //   }cout<<endl;
+ 	// cout<<"Penalty Weight: "<<endl;
+ 	// for(const double &u:weight){
+ 	// 	cout<<u<<" ";
+ 	// }cout<<endl;
+
+ 	// cout<<"Obj. Travel costs: "<<travelCosts<<endl;
+ 	// cout<<"Obj. Vehicle costs: "<<vehicleCosts<<endl;
+ 	// cout<<"Obj. Penalty costs: "<<penaltyCosts<<endl;
+
+    cout << "TRAVEL2: "<< travelCosts << "PENALTY2: " << penaltyCosts << "USE2: " << vehicleCosts <<endl;
+    return (travelCosts+vehicleCosts+penaltyCosts) ;
+}
+vector<int > calculatingDeliveryTime(const vector<data> &config, vector<int> &startVehicleTime, const vector<vector<int> > &time,const vector<int> &procTime,
+    const vector<vehicleLoaded> &vehicleOrder,int njobs){
+    vector<int> deliveryTime(njobs,0);
+    int acumulatedProcessTime=0;
+
+    // for(int i=0;i<vehicleOrder.size();i++){
+    // 	cout<<"Vehicle "<<vehicleOrder[i].id<<" carrega: ";
+
+    // 	for(int j=vehicleOrder[i].initialPos;j<=vehicleOrder[i].finalPos;j++){
+    // 		cout<<config[ j ].id<<" ";
+    // 	}
+    // 	cout<<endl;
+    // }
+    // cout<<"Processing time e: ";
+    // for(int u: procTime){
+    // 	cout<<u<<" ";
+    // }cout<<endl;
+
+
+    //Calculating the acumulated processing time for jobs and setting the starting time for vehicles.
+    for(int i=0;i<vehicleOrder.size();i++){
+        int whichCar=vehicleOrder[i].id; // Setting the delivery times by delivery sequence.
+        for(int j=vehicleOrder[i].initialPos;j<=vehicleOrder[i].finalPos;j++){
+            int whichJob=config[j].id;
+
+            acumulatedProcessTime += procTime[ whichJob ];
+        }
+        startVehicleTime[whichCar]=acumulatedProcessTime;
+
+
+    //Now calculating the travel time between delivered jobs
+        int acumulatedDeliveryTime=0;
+        int lastJob;
+        // Leaving origin to first job
+        acumulatedDeliveryTime = time[ 0 ][  config[vehicleOrder[i].initialPos].id +1];
+
+        for(int j=vehicleOrder[i].initialPos;j<vehicleOrder[i].finalPos;j++){
+            int whichJob=config[j].id;
+            int nextJob=config[j+1].id;
+
+            deliveryTime[ whichJob ]=acumulatedDeliveryTime + acumulatedProcessTime;
+            acumulatedDeliveryTime+= time[ whichJob+1 ][ nextJob+1];
+
+        }
+
+        deliveryTime[ config[vehicleOrder[i].finalPos].id  ] = acumulatedProcessTime + acumulatedDeliveryTime;
+
+    }
+
+    // cout<<"Delivery time foi: ";
+    // for(int u: deliveryTime){
+    // 	cout<<u<<" ";
+    // }cout<<endl;
+
+    return deliveryTime;
+}
+
+vector<vehicleLoaded> generateVehicleOrder(const vector<data> &config,int ncars){
+    vector<vehicleLoaded> vehicleList;
+    for(int i=0;i<config.size();i++){
+        if(!config[i].job){ // We found a car, so lets update the jobs it carries
+            vehicleLoaded temp; //Temporary variable
+            temp.id=config[i].id;//Which car I found.
+            int count = i+1;
+            temp.initialPos=count;
+            while(count<config.size() && config[count].job){
+                count++;
+            }
+            temp.finalPos=count-1; // Final position in array of config which we found a job for this vehicle.
+            if(! (count == (i+1)) ){ //This vehicle doesnt carry any job. Ignore it.
+                vehicleList.push_back(temp);
+            }
+            i=count-1;
+        }
+
+    }
+    return vehicleList;
+}
+
+vector<int> calculatingJobTardiness(const vector<int> &deliveryTime,int njobs, const vector<int> &dueDate){
+    vector<int> T(njobs);
+    for(int i=0;i<njobs;i++){
+        T[i]=deliveryTime[i]-dueDate[i];
+
+    	if(T[i]<0)
+    		T[i]=0;
+    }
+    return T;
+}
+
+
+
 // Complexity: O( N + (K+N) ) = O(N)
 double calculateObj(Solution &S, int N, int K, const vector<int> &P, const vector<int> &d, const vector<int> &s,
                     const vector<double> &w, const vector<int> &Q, const vector<int> &F,
                     const vector<vector<int>> &t)
 {
-    unordered_map<int, vector<int>> Schedule; // * First: Vehicle ID , Sec: Jobs
+    unordered_map<int, vector<int>> Schedule; // * First: Vehicle ID, Sec: Jobs
+    unordered_map<int, int> JobPath; // * First: JobId, Sec: Vehicle ID
     unordered_map<int, int> accPTime; // * First: Vehicle ID, Sec: acumulated P 
-
+    
     unordered_set<int> Vehicle;
     vector<int> Order;
 
@@ -53,13 +216,21 @@ double calculateObj(Solution &S, int N, int K, const vector<int> &P, const vecto
     UseCosts = TravelCosts = PenaltyCosts = OverlapCosts = 0.0;
 
     for(int i=0; i<N; i++){
-        Schedule[ S.M[0][i] ].push_back( S.M[1][i] );
+
+        accPTime[ S.M[0][i] ] += P[ i ];
+        
+        JobPath[i+1] = S.M[0][i];
+
+    }
+
+    for(int i=0; i<N; i++){
+
+        Schedule[ JobPath[ S.M[1][i]] ].push_back( S.M[1][i] );
 
         if(Vehicle.find(S.M[0][i]) == Vehicle.end()){
             Order.push_back( S.M[0][i] );
             Vehicle.insert( S.M[0][i] );
         }
-        accPTime[ S.M[0][i] ] += P[ S.M[1][i]-1 ];
     }
 
     for(int &idV: Order){
@@ -98,6 +269,84 @@ double calculateObj(Solution &S, int N, int K, const vector<int> &P, const vecto
     // cout << "UseCosts: " << UseCosts << "\n";
     // cout << "PenaltyCosts: " << PenaltyCosts << "\n";
     // cout << "OverlapCosts: " << OverlapCosts << "\n";
+
+    if(OverlapCosts == 0){
+        
+        for(int i=0;i<N;i++){
+            cout<<"V"<<S.M[0][i]-1<<" ";
+        }cout<<endl;
+        for(int i=0;i<N;i++){
+            cout<<"J"<<S.M[1][i]-1<<" ";
+        }cout<<endl;
+        cout<<"TRAVEL: "<< TravelCosts << "PENALTY: " << PenaltyCosts << "USE: " << UseCosts << "\n\n";
+
+        vector<data> initialConfig;
+        unordered_set<int> yes;
+        
+        unordered_map<int, unordered_set<int>> carregoBurro;
+        vector<int> orderBurro;
+        for(int i=0; i<N; i++){
+            int v = S.M[0][i]-1;
+            carregoBurro[ v ].insert(i);
+            if(yes.find(v) == yes.end()){
+                orderBurro.push_back(v);
+                yes.insert(v);
+            }
+        }
+        for(int i=0; i<orderBurro.size(); i++){
+            cout << "veiculo "<< orderBurro[i] << " leva: ";
+            auto it = carregoBurro[ orderBurro[i]].begin();
+            while(it != carregoBurro[orderBurro[i]].end()  )
+            {
+                cout<< *it <<" ";
+                it++;
+            }cout << endl;
+        }
+
+        for(int i=0; i<orderBurro.size(); i++){
+            int v = orderBurro[i];
+            data Go1; Go1.job =false; Go1.id = v;
+            initialConfig.push_back( Go1 );
+            for(int j=0; j<N; j++){
+                if( !(carregoBurro[ v ].find(S.M[1][j]-1) == carregoBurro[v].end()) ){
+                    data Go2; Go2.job = true; Go2.id = S.M[1][j]-1;
+                    initialConfig.push_back( Go2 );
+                }
+            }
+        }
+
+
+        //complete
+        for(int i=0;i<K; i++){
+            if(yes.find(i) == yes.end()){
+                data Go;
+                Go.id = i;
+                yes.insert(i);
+                Go.job = false;
+                initialConfig.push_back( Go );
+            }
+        }
+        assert( yes.size() == K );
+        
+        for(int i=0;i<initialConfig.size();i++){
+            if(initialConfig[i].job){
+                cout<<"J"<<initialConfig[i].id<<" ";
+            }else{
+                cout<<"V"<<initialConfig[i].id<<" ";
+            }
+        } cout << endl;
+
+         vector<vehicleLoaded> vehicleOrder=generateVehicleOrder(initialConfig, K);//Generating info about vehicle transportation
+
+        vehicleOrder=generateVehicleOrder(initialConfig, K);//Generating info about vehicle transportation
+        vector<int> startVehicleTime(K,0);//Calculating Delivery time(D) and Starting time(Sk)
+        vector<int> deliveryTime = calculatingDeliveryTime(initialConfig,startVehicleTime,t,P,vehicleOrder, N);
+        vector<int> jobTardiness = calculatingJobTardiness(deliveryTime, N,d);//Generating Job Tardiness (T) of each job (O(N))
+        double finalObj= objFunction(vehicleOrder,initialConfig, K, N,t,w,jobTardiness,F);
+
+        cerr <<"FIRST: " << (TravelCosts + UseCosts + PenaltyCosts) << "SECOND: " << finalObj << "\n\n";
+        assert( (TravelCosts + UseCosts + PenaltyCosts) == finalObj );
+    }
     return UseCosts + TravelCosts + PenaltyCosts + OverlapCosts;
 }
 
