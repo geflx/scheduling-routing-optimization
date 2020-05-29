30d16904c9ff026ca3e0dedc8ebb9fcc9749363a
geflx <gabrielfelix2500@gmail.com>
2020-05-29 16:19:58 -0300

    Formatting Op System

--- Code/AG2-Paper/ag2.cpp
+++ Code/AG2-Paper/ag2.cpp
@@ -28,26 +28,36 @@ int main(){
 
         for(int i=0; i<runNb; i++){
             
-            /*
+            time_t iniTime, endTime;
+            time(&iniTime);
+
             Solution S;
             if( gaVersion == 1)
                 S = GA_Version_1(N, K, itNumber, popSize, P, d, s, w, Q, F, t);
             else if( gaVersion == 2)
                 S = GA_Version_2(N, K, itNumber, popSize, P, d, s, w, Q, F, t);
                 
-            printSolution(S, N);
-            */
-            Solution S;
-            S = GA_Version_2(N, K, itNumber, popSize, P, d, s, w, Q, F, t);
+            //printSolution(S, N);
+            
+
+
+            // Solution S = GA_Version_2(N, K, itNumber, popSize, P, d, s, w, Q, F, t);
+
+            time(&endTime);
+            double timeSpent = difftime(endTime, iniTime);
 
             pair<double, double> Ans = tempObj(S, N, K, P, d, s, w, Q, F, t);
-            cout << Ans.first << " " << Ans.first + Ans.second << " " ;
-            if( Ans.second > 0.1){
-                cout << Ans.first * Ans.second;
-            }else{
-                cout << -1 ;
-            }
-            cout << " " << Ans.second << "\n";
+            
+            double multiplier = 0;
+
+            if(N == 8 || N == 10)
+                multiplier = 100;
+            else if( N ==15 || N == 20)
+                multiplier = 500;
+            else if( N >= 50 && N <= 100)
+                multiplier = 1000;
+            
+            cout << Ans.first + multiplier * Ans.second << " " << timeSpent << "\n";
             
         } 
     }
